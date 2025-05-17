ca16ce31db87182dce3a32b72cd572ff969c5969
Ziv <ZivTaly@post.bgu.ac.il>
2025-05-17 15:59:24 +0300

    update the applcaition to support working with Subebase

---
+++ prisma/migrations/20250516_update_ssl/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE IF NOT EXISTS "GroceryItem" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'toBuy',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "GroceryItem_pkey" PRIMARY KEY ("id")
+); 
